4bf0fdd0e4ce97df75bd4ff134238f0701f46a91
dit03 <dit@imac-4.local>
2019-03-20 16:42:40 +0900

    First Commit

--- Count/ViewController.swift
+++ Count/ViewController.swift
@@ -10,11 +10,39 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var count: UILabel!
+    var counter = 0
+    var dire = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        count.text = String(counter)
     }
-
+    @IBAction func btnTap(_ sender: Any) {
+        if dire == false {
+            if counter < 9 {
+                counter += 1
+                if counter == 9 {
+                    dire = true
+                }
+            }
+        } else {
+            if counter > 0 {
+                counter -= 1
+                if counter == 0 {
+                dire = false
+            }
+        }
+        }
+        
+        
+        
+        
+        
+        count.text = String(counter)
+    }
+    
 
 }
 
